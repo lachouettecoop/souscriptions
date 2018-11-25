15a3a2bd3438d777fbbf7209711264ecd8cb4a4d
Pierre Martin <pierre@occitech.fr>
2018-11-25 01:14:53 +0100

    feat(style): mise en page du tableau de bord
    en réutilisant les composants actuels

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Box, Heading, Text } from "rebass";
 import Auth from "./Auth";
+import PageHead from "./ui/PageHead";
+import Container from "./ui/Container";
 
 const Dashboard = () => {
   return (
     <Auth>
       {user => (
         <Box>
-          <Heading>Bonjour {user.displayName}</Heading>
-          <Text as="p">Votre token est : {user.token}</Text>
-          <pre>{JSON.stringify(user, null, 2)}</pre>
+          <PageHead title={`Bonjour ${user.firstname},`}>
+            <Text as="p">
+              Cette page contient les informations dont vous devriez avoir
+              besoin. Elle vous permet également d’accéder aux différentes
+              démarches qu’il est possible de faire en ligne.
+            </Text>
+          </PageHead>
+          <Container>
+            <Text as="p">Votre token est : {user.token}</Text>
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+          </Container>
         </Box>
       )}
     </Auth>
